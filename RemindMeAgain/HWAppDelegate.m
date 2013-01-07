bd8f8cfce9bdbfd346c71d7eaaf92938728d27e6
kavan puranik <kavanpuranik@gmail.com>
2013-01-06 17:58:54 -0800

    pre-populating form fields from preferences

--- RemindMeAgain/HWAppDelegate.m
+++ RemindMeAgain/HWAppDelegate.m
@@ -10,14 +10,39 @@
 
 @implementation HWAppDelegate
 
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-    
-    [reminderTextField setStringValue:@"Get up. Take a Deep Breath. Stretch your legs."];
     
     [self initDatePicker];
+    
+    [self initFormFields];
+
     NSLog(@"application started");
+}
+
+- (void)initFormFields {
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    NSString *reminderText = [prefs stringForKey:@"reminderText"];
+    if (reminderText == nil){
+        reminderText = @"Get up. Take a Deep Breath. Stretch your legs.";
+    }
+    [reminderTextField setStringValue:reminderText];
+    
+    NSInteger reminderPeriodInSeconds = [prefs integerForKey:@"reminderPeriod"];
+    if (reminderPeriodInSeconds == 0){
+        reminderPeriodInSeconds = 15 * 60;
+    }
+    [self setReminderPeriod:reminderPeriodInSeconds];
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)notification {
+    
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    [prefs setObject:[reminderTextField stringValue] forKey:@"reminderText"];
+    [prefs setInteger:[self getReminderPeriod] forKey:@"reminderPeriod"];
+    [prefs synchronize];
+    
+    NSLog(@"application ended");
 }
 
 - (IBAction)startStopReminder:(id)sender {
@@ -37,31 +62,20 @@
     
     // Cancel any preexisting timer
     [self.repeatingTimer invalidate];
-    
-    NSDate *periodDate = [periodPicker dateValue];
-    NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *components = [calendar components:(NSHourCalendarUnit | NSMinuteCalendarUnit) fromDate:periodDate];
-    NSInteger hours = [components hour];
-    NSInteger minutes = [components minute];
-    NSInteger intervalInSeconds = hours * 60 * 60 + minutes * 60;
-    
-    NSLog(@"hours %ld minutes %ld", hours, minutes);
-    NSLog(@"Interval set to %ld seconds", intervalInSeconds);
-    
+
+    NSInteger intervalInSeconds = [self getReminderPeriod];    
     NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval: intervalInSeconds
                                                       target:self selector:@selector(startReminder:)
                                                     userInfo:[self userInfo] repeats:YES];
     
-    self.repeatingTimer = timer;
-    
-    NSLog(@"Started Timer...");
+    self.repeatingTimer = timer;        
     [statusLabel setStringValue: @"Reminder is turned on."];
+    NSLog(@"Started Timer...");
 }
 
 - (void)startReminder:(NSTimer*)theTimer {
     NSDate *startDate = [[theTimer userInfo] objectForKey:@"StartDate"];
-    NSLog(@"Timer started on %@", startDate);
-    
+    NSLog(@"Notifying at %@", startDate);    
     NSString* reminderText = [reminderTextField stringValue];
     
     NSUserNotification *notification = [[NSUserNotification alloc] init];
@@ -75,7 +89,7 @@
 - (void) stopReminderTimer {
     [self.repeatingTimer invalidate];
     self.repeatingTimer = nil;
-    NSLog(@"Ended Timer.");
+    NSLog(@"Stopping timer.");
 }
 
 - (NSDictionary *)userInfo {
@@ -93,6 +107,32 @@
      [periodPickerCell setLocale:my24HourLocale];
      [periodPickerCell setFormatter:[[NSDateFormatter alloc] initWithDateFormat:@"%H:%M" allowNaturalLanguage:NO] ];
      [periodPickerCell setTitle:@"00:30"];
+}
+
+- (NSInteger)getReminderPeriod {
+    NSDate *periodDate = [periodPicker dateValue];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [calendar components:(NSHourCalendarUnit | NSMinuteCalendarUnit) fromDate:periodDate];
+    NSInteger hours = [components hour];
+    NSInteger minutes = [components minute];
+    NSInteger intervalInSeconds = hours * 60 * 60 + minutes * 60;
+    
+    NSLog(@"hours %ld minutes %ld", hours, minutes);
+    NSLog(@"Interval set to %ld seconds", intervalInSeconds);
+    return intervalInSeconds;
+}
+
+- (void)setReminderPeriod:(NSInteger)periodInSeconds {
+    NSInteger reminderPeriodInMinutes = periodInSeconds / 60;
+    NSLog(@"Reminder in minutes is %ld", reminderPeriodInMinutes);
+    
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    [dateComponents setMinute: reminderPeriodInMinutes % 60];
+    [dateComponents setHour: reminderPeriodInMinutes / 60];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDate* result = [calendar dateFromComponents:dateComponents];
+    
+    [periodPicker setDateValue:result];
 }
 
 @end
